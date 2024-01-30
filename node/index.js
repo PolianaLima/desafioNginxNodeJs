f9b0f7fd18e78c37bb2d14415923cf9285b49531
Poliana Lima <pollyannati@hotmail.com>
2024-01-30 17:28:41 -0300

    Desafio Nginx

---
+++ node/index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+ const config = {
+    host: 'db',
+    user:'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql2')
+const connection = mysql.createConnection(config)
+
+/* const deleteSql = `DELETE FROM people`
+connection.query(deleteSql) */
+
+const sql = `INSERT INTO people(name) values('Poliana'), ('Andre'), ('Alice')`
+connection.query(sql) 
+
+app.get('/people', (req, res) => {
+    const sql = 'SELECT * FROM people';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        res.json(results);
+
+        connection.end();
+    });
+});
+
+app.get('/', (req, res)=>{
+
+    const sql = 'SELECT name FROM people';
+
+     connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        // Mapeia os resultados para obter apenas os nomes
+        const names = results.map(result => result.name);
+
+        const htmlResponse = `
+            <h1>Full Cycle</h1>
+            <p>Nomes cadastrados: ${names.join(', ')}</p>
+        `
+        
+           res.send(htmlResponse)
+        ;
+    }) 
+;
+
+
+});
+
+app.listen(port, ()=>{
+    console.log("Rodando na porta" + port);
+})
